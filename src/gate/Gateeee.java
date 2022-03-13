d6c4f394c76717a02225c59cd9ef10d76be50343
Madhu <boppa226@gmail.com>
2022-03-13 23:05:23 +0530

    commit on 13.3.2022 11.00PM

--- src/gate/Gateeee.java
+++ src/gate/Gateeee.java
@@ -1,17 +1,94 @@
 package gate;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.css.model.SelectorList;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class Gateeee {
+	
 
-	@Test
-	public void gateexam2022()
+	@Test    (enabled=false)  //url check
+	public void mainurl()
 	{
-		System.out.println("in gate class gateexam 2022");
+		
+		System.setProperty("webdriver.chrome.driver","D:\\ace-testing\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.aceenggacademy.com");
+			driver.manage().window().maximize();
+			System.out.println("title of page is: " + driver.getTitle());
+			driver.quit();
+			System.out.println("Main url working");
 	}
-	@Test
-	public void whygate()
+	
+	
+	
+	@Test    (enabled=true)   //url check
+	public void aboutus() throws InterruptedException
 	{
-		System.out.println("in gate class why gate");
+		
+		System.setProperty("webdriver.chrome.driver","D:\\ace-testing\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.aceenggacademy.com");
+			driver.manage().window().maximize();
+			Thread.sleep(3000);
+			//driver.findElement(By.partialLinkText("ABOUT")).click();
+			Select dropdown=new Select(driver.findElement(By.partialLinkText("ABOUT")));
+			Thread.sleep(2000);
+			dropdown.selectByVisibleText("ABOUT ACE");
+			
+			//Thread.sleep(2000);
+			//driver.findElement(By.partialLinkText("ABOUT")).click();
+			
+			//System.out.println(dropdown.selectByVisibleText("ABOUT ACE"));
+			//System.out.println("title of page is: " + driver.getTitle());
+			
+			driver.quit();
+			
+		System.out.println("about link working properly");
+	}
+	
+	
+	
+	
+	
+	@Test    (enabled=false)   //
+	public void enrollement()
+	{
+		
+		System.setProperty("webdriver.chrome.driver","D:\\ace-testing\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.aceenggacademy.com");
+			driver.manage().window().maximize();
+			
+			driver.findElement(By.linkText("ENROLLMENT")).click();
+			System.out.println("title of page is: " + driver.getTitle());
+			
+			driver.quit();
+			
+		System.out.println("Enrollement link working properly");
+	}
+	
+	
+	
+	
+	
+	
+	@Test    (enabled=false)   //gate tap check
+	public void postal()
+	{
+		System.setProperty("webdriver.chrome.driver","D:\\ace-testing\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.aceenggacademy.com");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.partialLinkText("POSTAL")).click();
+		System.out.println("title of page is: " + driver.getTitle());
+		
+		driver.quit();
+		
+	System.out.println("POSTAL COACHING link working properly");
 	}
 }
