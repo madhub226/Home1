5535a90dbbe02fc09e0d508b85843dd4a06b0e84
Madhu <boppa226@gmail.com>
2022-03-13 09:34:16 +0530

    second commit

---
+++ src/ese/ese.java
@@ -0,0 +1,19 @@
+package ese;
+
+import org.testng.annotations.Test;
+
+public class ese {
+	
+	
+	@Test
+	public void eseexam2022()
+	{
+		System.out.println("in ese class eseexam 2022");
+	}
+	@Test
+	public void whyese()
+	{
+		System.out.println("in ese class why ese");
+	}
+
+}
---
+++ src/gate/Gateeee.java
@@ -0,0 +1,17 @@
+package gate;
+
+import org.testng.annotations.Test;
+
+public class Gateeee {
+
+	@Test
+	public void gateexam2022()
+	{
+		System.out.println("in gate class gateexam 2022");
+	}
+	@Test
+	public void whygate()
+	{
+		System.out.println("in gate class why gate");
+	}
+}
